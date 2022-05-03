ef955134b3dcc7d68b04d0cf43a01d0dab314e5d
Evelyn Fernandes <evelyn.af22@gmail.com>
2022-05-03 11:37:25 -0300

    Classes modelos criadas para a aplicação

---
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -0,0 +1,22 @@
+package com.dio.santander.bankline.api.model;
+
+public class Conta {
+	private long numero;
+	private Double saldo;
+	
+	public long getNumero() {
+		return numero;
+	}
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
